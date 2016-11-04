cd40ec15ab03e7882ce5e92f5fc0705bd7e213fc
Wei-Chung Huang <wayne@Waynes-MacBook-Pro.local>
2016-11-04 12:25:22 -0700

    test to add cpp files

---
+++ test.cpp
@@ -0,0 +1,9 @@
+//
+//  test.cpp
+//  CS255_final
+//
+//  Created by Wayne Huang on 11/4/16.
+//
+//
+
+#include "test.hpp"
---
+++ test.hpp
@@ -0,0 +1,14 @@
+//
+//  test.hpp
+//  CS255_final
+//
+//  Created by Wayne Huang on 11/4/16.
+//
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
